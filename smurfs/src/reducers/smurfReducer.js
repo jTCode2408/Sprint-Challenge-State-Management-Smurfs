d1457edcdac37161c09e0a30dda64b16c31b8eea
Jasmine Terry <jasmine.terry91@gmail.com>
2020-01-24 12:02:50 -0500

    actions added to reducer

--- smurfs/src/reducers/smurfReducer.js
+++ smurfs/src/reducers/smurfReducer.js
@@ -1,4 +1,5 @@
 //reducer for handling smurfs returning
+import {FETCHING_SMURF_FAIL, FETCHING_SMURF_SUCCESS, FETCHING_SMURF_START} from '../actions/smurfActions';
 
 const initialState = {
 loading:false,
@@ -9,6 +10,27 @@ error: ''
 
 export const smurfReducer = (state= initialState, action)=>{
 switch(action.type){
+    case FETCHING_SMURF_START:
+        return{
+            ...state,
+            loading:true
+        };
+
+        case FETCHING_SMURF_SUCCESS:
+            return{
+                ...state, 
+                loading: false,
+                gettingSmurfs: action.payload
+
+            };
+
+            case FETCHING_SMURF_FAIL:
+                return{
+                    ...state,
+                    loading: null,
+                    gettingSmurfs: null,
+                    error: 'SOMETHING WENT WRONG'
+                }
 default:
     return state
 }
